decc554b0786dd4c1a6a799f73639dbb1941c455
Alvaro Dans Montilla <alvaro.dans.montilla@es.ey.com>
2025-06-05 00:41:43 +0200

    fix: payment management config

---
+++ payment-management/src/main/java/com/tfg/payment/infrastructure/config/redis/RedisConfig.java
@@ -0,0 +1,27 @@
+package com.tfg.payment.infrastructure.config.redis;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory("localhost", 6379);
+    }
+
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory cf) {
+        return new StringRedisTemplate(cf);
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+}
